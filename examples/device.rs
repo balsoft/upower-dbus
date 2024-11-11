96ae420bc19131f1890f14baf848f19c5b4ba69d
Alexander Bantyev <balsoft@balsoft.ru>
2024-11-11 19:31:25 +0400

    Update device.rs example to be more complete

--- examples/device.rs
+++ examples/device.rs
@@ -3,7 +3,7 @@
 
 extern crate upower_dbus;
 
-use upower_dbus::UPowerProxy;
+use upower_dbus::{UPowerProxy, DeviceProxy, BatteryState};
 
 fn main() -> zbus::Result<()> {
     futures::executor::block_on(async move {
@@ -11,15 +11,24 @@ fn main() -> zbus::Result<()> {
 
         let upower = UPowerProxy::new(&connection).await?;
 
-        let device = upower.get_display_device().await?;
-
-        println!("BatteryLevel: {:?}", device.battery_level().await);
-        println!("IconName: {:?}", device.icon_name().await);
-        println!("IsPresent: {:?}", device.is_present().await);
-        println!("Online: {:?}", device.online().await);
-        println!("Percentage: {:?}", device.percentage().await);
-        println!("State: {:?}", device.state().await);
-        println!("Type: {:?}", device.type_().await);
+        for bat in upower.enumerate_devices().await? {
+            let device = DeviceProxy::new(&connection, bat).await?;
+            println!("Device: {:?}", device.native_path().await?);
+            println!("\tType: {:?}", device.type_().await?);
+            println!("\tPercentage: {:?}", device.percentage().await?);
+            println!("\tBatteryLevel: {:?}", device.battery_level().await?);
+            println!("\tWarningLevel: {:?}", device.warning_level().await?);
+            println!("\tIconName: {:?}", device.icon_name().await?);
+            println!("\tIsPresent: {:?}", device.is_present().await?);
+            println!("\tOnline: {:?}", device.online().await?);
+            println!("\tState: {:?}", device.state().await?);
+            if let BatteryState::Charging = device.state().await? {
+                println!("\tTimeToFull: {:?}", device.time_to_full().await?);
+            }
+            if let BatteryState::Discharging = device.state().await? {
+                println!("\tTimeToEmpty: {:?}", device.time_to_empty().await?);
+            }
+        };
 
         Ok(())
     })
